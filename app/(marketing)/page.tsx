6cfe23b0bfa672638161009176207369f380b83e
khangp11 <vkhang261@gmail.com>
2025-03-26 21:34:31 +0700

    init source code

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link"
+
+import { env } from "@/env.mjs"
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+
+async function getGitHubStars(): Promise<string | null> {
+  try {
+    const response = await fetch(
+      "https://api.github.com/repos/shadcn/taxonomy",
+      {
+        headers: {
+          Accept: "application/vnd.github+json",
+          Authorization: `Bearer ${env.GITHUB_ACCESS_TOKEN}`,
+        },
+        next: {
+          revalidate: 60,
+        },
+      }
+    )
+
+    if (!response?.ok) {
+      return null
+    }
+
+    const json = await response.json()
+
+    return parseInt(json["stargazers_count"]).toLocaleString()
+  } catch (error) {
+    return null
+  }
+}
+
+export default async function IndexPage() {
+  const stars = await getGitHubStars()
+
+  return (
+    <>
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+            Ở đây chưa có gì cả
+          </h1>
+        </div>
+      </section>
+    </>
+  )
+}
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "Anh Thu",
+  description:
+    "Next.js is a React framework that enables functionality such as server-side rendering and generating static websites for React based web applications.",
+  url: "https://tx.shadcn.com",
+  ogImage: "https://tx.shadcn.com/og.jpg",
+  links: {
+    twitter: "https://www.instagram.com/khag_02/",
+    github: "https://github.com/shadcn/taxonomy",
+  },
+}
